0e744dee0be34450cbb94164ab05e057b2893445
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2024-01-09 12:59:04 +0700

    [FIX] Helper Ambil Item Transfer

--- resources/views/app/dashboard/helper/_reload_transfer_invoice.blade.php
+++ resources/views/app/dashboard/helper/_reload_transfer_invoice.blade.php
@@ -1,4 +1,4 @@
-<select class="form-control" id="scan_transfer_invoice" name="scan_transfer_invoice">
+<select class="form-control" id="transfer_invoice" name="transfer_invoice">
     <option value="">PILIH INVOICE</option>
     @foreach ($data['invoice'] as $key => $value)
         <option value="{{ $key }}">{{ $value }}</option>
