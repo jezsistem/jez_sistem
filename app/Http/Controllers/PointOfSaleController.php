0b7d7fab2be3bd8a41c5bf439609f11338d24e3e
Muhammad Royyan Zamzami <36367976+muhammadroyyan11@users.noreply.github.com>
2024-06-24 12:40:00 +0700

    [POS] - Change Search SKU

--- app/Http/Controllers/PointOfSaleController.php
+++ app/Http/Controllers/PointOfSaleController.php
@@ -1607,7 +1607,7 @@ class PointOfSaleController extends Controller
                         $bin = '<span class="btn-lg btn-info">' . strtoupper($row->pl_code) . '</span>';
                     } else {
                         $status = '';
-                        $bin = '<span class="btn-lg btn-info">asd</span>';
+                        $bin = '<span class="btn-lg btn-info">' . $row->pls_qty . ' ' . $row->pl_id . '</span>';
                     }
                     $output .= '
                     <li>
