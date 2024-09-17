8eab7aae87184be3c7dde17e96d0a55e574c42ea
Muhammad Royyan Zamzami <36367976+muhammadroyyan11@users.noreply.github.com>
2024-09-17 17:16:47 +0700

    [Fix Customer view] - Reload datatables

--- storage/framework/views/077904c592cf780d8a9b7e99ef70767104862aa6.php
+++
@@ -1,7 +0,0 @@
-<select class="form-control" id="pl_id" name="pl_id" required>
-    <option value="">- Pilih BIN -</option>
-    <?php $__currentLoopData = $data['pl_id']; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <option value="<?php echo e($key); ?>"><?php echo e($value); ?></option>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</select>
-<div id="pl_id_parent"></div><?php /**PATH /home/jezpro.id/public_html/resources/views/app/adjustment/_reload_location.blade.php ENDPATH**/ ?>
