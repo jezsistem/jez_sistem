4f2613821aa31e61cb440f68eb50f7bb694b1b3d
Muhammad Royyan Zamzami <36367976+muhammadroyyan11@users.noreply.github.com>
2024-09-18 23:36:03 +0700

    [Transaksi Online] - Fix Bug Print

--- app/Http/Controllers/TransaksiOnlineController.php
+++ app/Http/Controllers/TransaksiOnlineController.php
@@ -282,7 +282,7 @@ class TransaksiOnlineController extends Controller
         }
         $sku_count = OnlineTransactionDetails::where('order_number', $invoice)->count();
 
-        if(count($online_transactions) >= $sku_count){
+        if(count($online_transactions) == $sku_count){
             foreach ($online_transactions as $transaction) {
                 // Access individual fields from the $transaction array
                 $paramsPlst = [
