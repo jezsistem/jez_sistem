b4bd6deb49156dd03d2eae6ddb25b52fac8edec5
ADINDA RAHAJENG SILVIA PRANESTI <92132467+AdindaRahajengSilviaPranesti@users.noreply.github.com>
2024-10-03 10:12:48 +0700

    pull

--- app/Http/Controllers/KategoriPosV2Controller.php
+++ app/Http/Controllers/KategoriPosV2Controller.php
@@ -24,11 +24,7 @@ class KategoriPosV2Controller extends Controller
             'title'   => 'Kategori'
         ];
 
-<<<<<<< HEAD
         return view('app.posv2.kategori-posv2', $data_page);
-=======
-        return view('app.posv2.masterdata.kategori-posv2', $data_page);
->>>>>>> afb666ece44c3f16d1024e19263d7f58ba0983e1
     }
 
     public function datatable(Request $request)
@@ -146,9 +142,4 @@ class KategoriPosV2Controller extends Controller
             }
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
-    
->>>>>>> afb666ece44c3f16d1024e19263d7f58ba0983e1
