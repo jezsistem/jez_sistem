14e973b8db6547fd557161bb1b84738469d28033
Muhammad Royyan Zamzami <36367976+muhammadroyyan11@users.noreply.github.com>
2024-10-30 14:25:15 +0700

    [Invoice Editor] - Refund and cancel create new TRX

--- app/Http/Controllers/InvoiceEditorController.php
+++ app/Http/Controllers/InvoiceEditorController.php
@@ -747,7 +747,7 @@ class InvoiceEditorController extends Controller
                     if ($plst) {
                         foreach ($plst as $plr) {
                             DB::table('product_location_setup_transactions')->where('id', '=', $plr->id)->update([
-                                'plst_status' => 'INSTOCK',
+                                'plst_status' => 'WAITING OFFLINE',
                                 'updated_at' => date('Y-m-d H:i:s')
                             ]);
 
