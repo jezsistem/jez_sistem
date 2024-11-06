7df6c22c941071ec48fc43acc63dd52e0b462262
faradishaldina <faraldyna@gmail.com>
2024-11-06 15:48:30 +0700

    [Approval Penerimaan] - No Po, Tipe Stok, Pajak, Total Price

--- app/Http/Controllers/POReceiveApprovalController.php
+++ app/Http/Controllers/POReceiveApprovalController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\WebConfig;
 use App\Models\User;
+use App\Models\PurchaseOrder;
 
 class POReceiveApprovalController extends Controller
 {
@@ -91,8 +92,28 @@ class POReceiveApprovalController extends Controller
     {
         if (request()->ajax()) {
             return datatables()->of(DB::table('purchase_order_article_detail_statuses')
-                ->selectRaw("ts_purchase_order_article_detail_statuses.id as id, st_name, po_invoice, poads_invoice, invoice_date, ts_purchase_order_article_detail_statuses.created_at, u_name, u_id_approve,
-                sum(ts_purchase_order_article_detail_statuses.poads_qty) as qty, acc_id, is_paid, ts_stores.id as st_id, ts_purchase_orders.id as po_id, ps_name, po_description, po_shipping_cost")
+                ->selectRaw("
+                    ts_purchase_order_article_detail_statuses.id as id,
+                    st_name,
+                    po_invoice,
+                    poads_invoice,
+                    invoice_date,
+                    ts_purchase_order_article_detail_statuses.created_at,
+                    u_name,
+                    u_id_approve,
+                    sum(ts_purchase_order_article_detail_statuses.poads_qty) as qty,
+                    acc_id,
+                    is_paid,
+                    ts_stores.id as st_id,
+                    ts_purchase_orders.id as po_id,
+                    ps_name,
+                    po_description,
+                    po_shipping_cost,
+                    ts_purchase_orders.stkt_id,
+                    ts_purchase_orders.tax_id,
+                    ts_stock_types.stkt_name,
+                    ts_taxes.tx_name
+                ")
                 ->leftJoin('users', 'users.id', '=', 'purchase_order_article_detail_statuses.u_id_receive')
                 ->leftJoin('purchase_order_article_details', 'purchase_order_article_details.id', '=', 'purchase_order_article_detail_statuses.poad_id')
                 ->leftJoin('purchase_order_articles', 'purchase_order_articles.id', '=', 'purchase_order_article_details.poa_id')
@@ -100,91 +121,91 @@ class POReceiveApprovalController extends Controller
                 ->leftJoin('product_suppliers', 'product_suppliers.id', '=', 'purchase_orders.ps_id')
                 ->leftJoin('stores', 'stores.id', '=', 'purchase_orders.st_id')
                 ->leftJoin('products', 'products.id', '=', 'purchase_order_articles.p_id')
-//                ->leftJoin('product_suppliers', 'product_suppliers.id', '=', 'purchase_orders.ps_id')
+                ->leftJoin('stock_types', 'stock_types.id', '=', 'purchase_orders.stkt_id') // Join for stkt_id
+                ->leftJoin('taxes', 'taxes.id', '=', 'purchase_orders.tax_id') // Join for tax_id
                 ->whereNotNull('poads_invoice')
-                ->groupBy('poads_invoice'))
-                ->editColumn('poads_invoice_show', function ($d) {
-                    return "<a class='btn btn-primary'>" . $d->poads_invoice . "</a>";
-                })
-                ->editColumn('invoice_date_show', function ($data) {
-                    return date('d/m/Y', strtotime($data->invoice_date));
-                })
-                ->editColumn('receive_date_show', function ($data) {
-                    return date('d/m/Y H:i:s', strtotime($data->created_at));
-                })
-                ->editColumn('u_receive', function ($data) {
-                    if (!empty($data->u_id_approve) && $data->acc_id == 93 && $data->is_paid == 0) {
-//                        return 'Diterima, Belum Dibayar';
-                        $name = DB::table('users')->where('id', '=', $data->u_id_approve)->first()->u_name;
-                        return '<span class="badge badge-primary">' . $name . '<br/> Diterima, Belum Dibayar</span>';
-                    } else if (!empty($data->u_id_approve)) {
-                        $name = DB::table('users')->where('id', '=', $data->u_id_approve)->first()->u_name;
-                        return '<span class="badge badge-success">' . $name . '<br/>' . date('d/m/Y H:i:s', strtotime($data->created_at)) . '</span>';
-                    } else {
-//                        return 'Menunggu Approval';
-                        return '<span class="badge badge-warning">Menunggu Approval</span>';
+                ->groupBy('poads_invoice')
+            )
+            ->editColumn('poads_invoice_show', function ($d) {
+                return "<a class='btn btn-primary'>" . $d->poads_invoice . "</a>";
+            })
+            ->editColumn('invoice_date_show', function ($data) {
+                return date('d/m/Y', strtotime($data->invoice_date));
+            })
+            ->editColumn('receive_date_show', function ($data) {
+                return date('d/m/Y H:i:s', strtotime($data->created_at));
+            })
+            ->editColumn('u_receive', function ($data) {
+                if (!empty($data->u_id_approve) && $data->acc_id == 93 && $data->is_paid == 0) {
+                    $name = DB::table('users')->where('id', '=', $data->u_id_approve)->first()->u_name;
+                    return '<span class="badge badge-primary">' . $name . '<br/> Diterima, Belum Dibayar</span>';
+                } else if (!empty($data->u_id_approve)) {
+                    $name = DB::table('users')->where('id', '=', $data->u_id_approve)->first()->u_name;
+                    return '<span class="badge badge-success">' . $name . '<br/>' . date('d/m/Y H:i:s', strtotime($data->created_at)) . '</span>';
+                } else {
+                    return '<span class="badge badge-warning">Menunggu Approval</span>';
+                }
+            })
+            ->rawColumns(['poads_invoice_show', 'u_receive'])
+            ->filter(function ($instance) use ($request) {
+                if (!empty($request->get('search'))) {
+                    $instance->where(function ($w) use ($request) {
+                        $search = $request->get('search');
+                        $w->orWhere('poads_invoice', 'LIKE', "%$search%")
+                            ->orWhere('po_invoice', 'LIKE', "%$search%")
+                            ->orWhere('po_description', 'LIKE', "%$search%")
+                            ->orWhere('article_id', 'LIKE', "%$search%")
+                            ->orWhereRaw('CONCAT(p_name," ",p_color) LIKE ?', "%$search%");
+                    });
+                }
+                if (!empty($request->get('filter_status'))) {
+                    if ($request->get('filter_status') == 'approve') {
+                        $instance->whereNotNull('u_id_approve');
                     }
-                })
-                ->rawColumns(['poads_invoice_show', 'u_receive'])
-                ->filter(function ($instance) use ($request) {
-                    if (!empty($request->get('search'))) {
-                        $instance->where(function ($w) use ($request) {
-                            $search = $request->get('search');
-                            $w->orWhere('poads_invoice', 'LIKE', "%$search%")
-                                ->orWhere('po_invoice', 'LIKE', "%$search%")
-                                ->orWhere('po_description', 'LIKE', "%$search%")
-                                ->orWhere('article_id', 'LIKE', "%$search%")
-                                ->orWhereRaw('CONCAT(p_name," ",p_color) LIKE ?', "%$search%");
-                        });
+    
+                    if ($request->get('filter_status') == 'wait') {
+                        $instance->whereNull('u_id_approve');
                     }
-                    if (!empty($request->get('filter_status'))) {
-                        if ($request->get('filter_status') == 'approve') {
-                            $instance->whereNotNull('u_id_approve');
-                        }
-
-                        if ($request->get('filter_status') == 'wait') {
-                            $instance->whereNull('u_id_approve');
-                        }
-
-                        if ($request->get('filter_status') == 'wait_cod') {
-                            $instance->where('acc_id', '=', 93)->where('is_paid', '=', 0);
-                        }
+    
+                    if ($request->get('filter_status') == 'wait_cod') {
+                        $instance->where('acc_id', '=', 93)->where('is_paid', '=', 0);
                     }
-                    if (!empty($request->get('filter_cabang'))) {
-                        if ($request->get('filter_cabang') == 'SURABAYA') {
-                            $instance->where('st_name', 'LIKE', '%SURABAYA%');
-                        }
-
-                        if ($request->get('filter_cabang') == 'MALANG') {
-                            $instance->where('st_name', 'LIKE', '%MALANG%');
-                        }
-
-                        if ($request->get('filter_cabang') == 'KEDIRI') {
-                            $instance->where('st_name', 'LIKE', '%KEDIRI%');
-                        }
-
-                        if ($request->get('filter_cabang') == 'JEMBER') {
-                            $instance->where('st_name', 'LIKE', '%JEMBER%');
-                        }
+                }
+                if (!empty($request->get('filter_cabang'))) {
+                    if ($request->get('filter_cabang') == 'SURABAYA') {
+                        $instance->where('st_name', 'LIKE', '%SURABAYA%');
                     }
-                    if (!empty($request->get('date'))) {
-                        $dateParts = explode('|', $request->get('date'));
-
-                        if (count($dateParts) == 2) {
-                            // Date range provided
-                            $instance->whereBetween('purchase_order_article_detail_statuses.created_at', [$dateParts[0], $dateParts[1]]);
-                        } elseif (
-                            count($dateParts) == 1
-                        ) {
-                            // Single date provided
-                            $instance->whereDate('purchase_order_article_detail_statuses.created_at', $dateParts[0]);
-                        }
+    
+                    if ($request->get('filter_cabang') == 'MALANG') {
+                        $instance->where('st_name', 'LIKE', '%MALANG%');
                     }
-                })
-                ->addIndexColumn()
-                ->make(true);
+    
+                    if ($request->get('filter_cabang') == 'KEDIRI') {
+                        $instance->where('st_name', 'LIKE', '%KEDIRI%');
+                    }
+    
+                    if ($request->get('filter_cabang') == 'JEMBER') {
+                        $instance->where('st_name', 'LIKE', '%JEMBER%');
+                    }
+                }
+                if (!empty($request->get('date'))) {
+                    $dateParts = explode('|', $request->get('date'));
+    
+                    if (count($dateParts) == 2) {
+                        // Date range provided
+                        $instance->whereBetween('purchase_order_article_detail_statuses.created_at', [$dateParts[0], $dateParts[1]]);
+                    } elseif (count($dateParts) == 1) {
+                        // Single date provided
+                        $instance->whereDate('purchase_order_article_detail_statuses.created_at', $dateParts[0]);
+                    }
+                }
+            })
+            ->addIndexColumn()
+            ->make(true);
         }
     }
+    
+
     // public function poReceiveDetail(Request $request)
     // {
     //     $po_id = $request->_po_id;
