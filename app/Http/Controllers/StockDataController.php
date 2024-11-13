2991dacc7d5073a47315d105da974d9a9a09c53b
Muhammad Royyan Zamzami <36367976+muhammadroyyan11@users.noreply.github.com>
2024-11-13 11:33:01 +0700

    [Data Stok] - Fix search SKU

--- app/Http/Controllers/StockDataController.php
+++ app/Http/Controllers/StockDataController.php
@@ -530,7 +530,8 @@ class StockDataController extends Controller
                         $instance->where(function ($w) use ($request) {
                             $search = $request->get('search_scan');
                             $w->orWhereRaw('CONCAT(br_name," ",p_name," ",p_color," ",sz_name) LIKE ?', "%$search%")
-                                ->orWhereRaw('ts_products.article_id LIKE ?', "%$search%");
+                                ->orWhereRaw('ts_products.article_id LIKE ?', "%$search%")
+                                ->orWhereRaw('ts_product_stocks.ps_barcode LIKE ?', "%$search%");
                         });
                     }
                 })
