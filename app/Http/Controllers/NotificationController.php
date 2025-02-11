afac60333cd6b495aa7d8a511862755fae87fd38
Muhammad Royyan Zamzami <36367976+muhammadroyyan11@users.noreply.github.com>
2025-02-11 20:23:33 +0700

    [Notification] - Refund

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
     public function fetchNotifications()
     {
         // Fetch unread notifications
-        $notifications = Notification::where('is_read', false)
+        $notifications = Notification::where('is_read', false)->where('stt_id', '=', Auth::user()->stt_id)
             ->orderBy('created_at', 'desc')
             ->limit(5)
             ->get();
