9eb33a26d287052253ddd89383ef5284c38ba2ad
JaluDwija37 <jaludwija37@gmail.com>
2025-04-14 20:47:31 +0700

    [PURCHASE ORDER PRE ORDER] - fix show aging &

--- routes/purchase_order.php
+++ routes/purchase_order.php
@@ -130,6 +130,7 @@ Route::middleware(['auth'])->group(function () {
     Route::post('cancel_pre_order', [PreOrderController::class, 'cancelPreOrder']);
     Route::post('pre_order_save_draft', [PreOrderController::class, 'poSaveDraft']);
     Route::post('check_pre_order_purchase_order', [PreOrderController::class, 'checkPreOrderPurchaseOrder']);
+    Route::get('pre_order_article_export', [PreOrderController::class, 'exportPreOrderArticleData']);
 
     // Purchase Order Article
     Route::post('proa_delete', [PreOrderArticleController::class, 'deleteData']);
