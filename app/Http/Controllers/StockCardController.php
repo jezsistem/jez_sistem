a28bb0402e40383c77d728ab01c7917e34ab0cea
muhammadroyyan11 <royyanmz87@gmail.com>
2025-05-27 15:02:02 +0700

    [Report Stok] - Testing Calling API

--- app/Http/Controllers/StockCardController.php
+++ app/Http/Controllers/StockCardController.php
@@ -500,7 +500,7 @@ class StockCardController extends Controller
                  ->addColumn('adj_plus', fn ($row) => $row->SO_adjustment_plus)
                  ->addColumn('ending_stock', fn ($row) => $row->ending_stocks)
                  ->addColumn('today_stock', fn ($row) => $row->today_stocks)
-g                 ->rawColumns(['article'])
+                ->rawColumns(['article'])
                  ->make(true);
          }
      }
