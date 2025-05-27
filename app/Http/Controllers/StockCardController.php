75f42cef835f0cb6c5b95ff848ec40f15910f0c6
muhammadroyyan11 <royyanmz87@gmail.com>
2025-05-27 15:23:44 +0700

    [Report Stok] - Filter Article ID

--- app/Http/Controllers/StockCardController.php
+++ app/Http/Controllers/StockCardController.php
@@ -476,15 +476,15 @@ class StockCardController extends Controller
 
              $startDate = $start;
              $endDate = $end;
-//             $article_id = '';
+             $article_id = '';
 
-             $input = $request->get('search');
+//             $input = $request->get('search');
 
-             if (!empty($input)) {
-                 $article_id = $input;
-             } else {
-                 $article_id = '';
-             }
+//             if (!empty($input)) {
+//                 $article_id = $input;
+//             } else {
+//                 $article_id = '';
+//             }
 
              $data = DB::select("CALL sumary_stocks(?, ?, ?, ?, ?)", [
                  $store,
