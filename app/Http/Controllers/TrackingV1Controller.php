29c693d883aff94d9862fe00951449a9f837d151
muhammadroyyan11 <royyanmz87@gmail.com>
2025-06-20 14:52:19 +0700

    merge from develop

--- app/Http/Controllers/TrackingV1Controller.php
+++ app/Http/Controllers/TrackingV1Controller.php
@@ -549,7 +549,7 @@ class TrackingV1Controller extends Controller
     public function scanOutDatatables(Request $request)
     {
         if (request()->ajax()) {
-            return datatables()->of(ProductLocationSetupTransaction::select('product_location_setup_transactions.id as plst_id', 'pls_id', 'pst_id', 'pls_qty', 'plst_qty', 'plst_status', 'pl_id', 'u_name', 'p_name', 'br_name', 'p_color', 'sz_name', 'pl_code', 'pl_name', 'pl_description', 'product_location_setup_transactions.created_at as plst_created', 'ps_barcode')
+            return datatables()->of(ProductLocationSetupTransaction::select('product_location_setup_transactions.id as plst_id', 'pls_id', 'product_location_setups.pst_id as pst_id', 'pls_qty', 'plst_qty', 'plst_status', 'pl_id', 'u_name', 'p_name', 'br_name', 'p_color', 'sz_name', 'pl_code', 'pl_name', 'pl_description', 'product_location_setup_transactions.created_at as plst_created', 'ps_barcode')
                 ->leftJoin('product_location_setups', 'product_location_setups.id', '=', 'product_location_setup_transactions.pls_id')
                 ->leftJoin('product_stocks', 'product_stocks.id', '=', 'product_location_setups.pst_id')
                 ->leftJoin('products', 'products.id', '=', 'product_stocks.p_id')
