70444da05dbedc3463e283192c09455bccb8a414
Alfan Zuhdi Assyura <alfanassyura@gmail.com>
2025-07-15 10:38:04 +0700

    [MutasiStock] - fix export in mutasi stock

--- app/Exports/SetupHistoryExport.php
+++ app/Exports/SetupHistoryExport.php
@@ -33,7 +33,7 @@ class SetupHistoryExport implements FromCollection, WithHeadings
 
     public function headings(): array
     {
-        return ["Tanggal", "Store", "User", "SKU", "Brand", "Artikel", "Warna", "Size", "BIN Awal", "QBIN Awal", "Qty Mts", "BIN Awal Setelah Mts", "BIN Tujuan"];
+        return ["Tanggal", "Store", "User", "SKU", "Brand", "Artikel", "Warna", "Size", "BIN Awal", "QBIN Awal", "Qty Mts", "BIN Awal Setelah Mts", "BIN Tujuan", "Catatan"];
     }
 
     public function collection()
@@ -44,85 +44,77 @@ class SetupHistoryExport implements FromCollection, WithHeadings
         $search = $this->search;
         $history_bin_start = $this->history_bin_start;
         $history_bin_end = $this->history_bin_end;
-
-        $export = array();
-
-        $data = ProductMutation::select('st_name', 'u_name', 'pmt_old_qty', 'pmt_qty', 'pl_code', 'pls_id', 'product_mutations.created_at')
-            ->leftJoin('product_locations', 'product_locations.id', '=', 'product_mutations.pl_id')
-            ->leftJoin('stores', 'stores.id', '=', 'product_locations.st_id')
+        
+        $data = DB::table('product_mutations')
+            ->select([
+                'product_mutations.created_at',
+                'stores.st_name',
+                'users.u_name',
+                'product_stocks.ps_barcode',
+                'brands.br_name',
+                'products.p_name',
+                'products.p_color',
+                'sizes.sz_name',
+                'pl_start.pl_code as bin_start',
+                'product_mutations.pmt_old_qty',
+                'product_mutations.pmt_qty',
+                DB::raw('(ts_product_mutations.pmt_old_qty - ts_product_mutations.pmt_qty) as bin_awal_setelah_mts'),
+                'pl_target.pl_code as bin_tujuan',
+                'product_mutations.notes'
+            ])
+            ->leftJoin('product_location_setups', 'product_mutations.pls_id', '=', 'product_location_setups.id')
+            ->leftJoin('product_locations as pl_target', 'product_mutations.pl_id', '=', 'pl_target.id') // BIN tujuan
+            ->leftJoin('product_locations as pl_start', 'product_location_setups.pl_id', '=', 'pl_start.id') // BIN awal
+            ->leftJoin('stores', 'stores.id', '=', 'pl_target.st_id')
             ->leftJoin('users', 'users.id', '=', 'product_mutations.u_id')
-            ->join('product_location_setups', 'product_mutations.pls_id', '=',  'product_location_setups.id')
-            ->join('product_stocks', 'product_location_setups.pst_id', '=', 'product_stocks.id')
-            ->join('products', 'products.id', '=', 'product_stocks.p_id')
-            ->where(function ($w) use ($st_id) {
-            if (!empty($st_id)) {
-                $w->where('product_locations.st_id', '=', $st_id);
-            }
+            ->leftJoin('product_stocks', 'product_location_setups.pst_id', '=', 'product_stocks.id')
+            ->leftJoin('products', 'products.id', '=', 'product_stocks.p_id')
+            ->leftJoin('sizes', 'sizes.id', '=', 'product_stocks.sz_id')
+            ->leftJoin('brands', 'brands.id', '=', 'products.br_id')
+            ->when(!empty($st_id), function ($query) use ($st_id) {
+                $query->where('pl_target.st_id', $st_id);
             })
-            ->where(function ($w) use ($start, $end) {
-            if (!empty($end)) {
-                $w->whereDate('product_mutations.created_at', '>=', $start)
-                  ->whereDate('product_mutations.created_at', '<=', $end);
-            } else {
-                $w->whereDate('product_mutations.created_at', $start);
-            }
+            ->when(!empty($start), function ($query) use ($start, $end) {
+                if (!empty($end)) {
+                    $query->whereBetween(DB::raw('DATE(ts_product_mutations.created_at)'), [$start, $end]);
+                } else {
+                    $query->whereDate('product_mutations.created_at', $start);
+                }
             })
-            ->where(function ($w) use ($search) {
-            if (!empty($search)) {
-                $w->orWhere('product_stocks.ps_barcode', 'LIKE', "%$search%")
-                  ->orWhere('users.u_name', 'LIKE', "%$search%")
-                  ->orWhere('products.p_name', 'LIKE', "%$search%");
-            }
+            ->when(!empty($search), function ($query) use ($search) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('product_stocks.ps_barcode', 'like', "%$search%")
+                        ->orWhere('users.u_name', 'like', "%$search%")
+                        ->orWhere('products.p_name', 'like', "%$search%");
+                });
             })
-            ->where(function ($w) use ($history_bin_start, $history_bin_end) {
-            if (!empty($history_bin_start)) {
-                $w->where('product_location_setups.pl_id', '=', $history_bin_start);
-            }
-            if (!empty($history_bin_end)) {
-                $w->where('product_mutations.pl_id', '=', $history_bin_end);
-            }
+            ->when(!empty($history_bin_start), function ($query) use ($history_bin_start) {
+                $query->where('product_location_setups.pl_id', '=', $history_bin_start);
+            })
+            ->when(!empty($history_bin_end), function ($query) use ($history_bin_end) {
+                $query->where('product_mutations.pl_id', '=', $history_bin_end);
             })
             ->get();
-        if (!empty($data->first())) {
-            foreach ($data as $row) {
-                $ar = ProductStock::select('p_name', 'br_name', 'sz_name', 'p_color', 'pl_code', 'ps_barcode as sku')
-                    ->leftJoin('products', 'products.id', '=', 'product_stocks.p_id')
-                    ->leftJoin('brands', 'brands.id', '=', 'products.br_id')
-                    ->leftJoin('sizes', 'sizes.id', '=', 'product_stocks.sz_id')
-                    ->leftJoin('product_location_setups', 'product_location_setups.pst_id', '=', 'product_stocks.id')
-                    ->leftJoin('product_locations', 'product_locations.id', '=', 'product_location_setups.pl_id')
-                    ->where('product_location_setups.id', $row->pls_id)
-                    ->first();
 
-                if (!$ar) {
-                    // If no result is found, assign default values or skip
-                    $br_name = $p_name = $p_color = $sz_name = $pl_code = $sku = 'N/A';
-                } else {
-                    $br_name = $ar->br_name;
-                    $p_name = $ar->p_name;
-                    $p_color = $ar->p_color;
-                    $sz_name = $ar->sz_name;
-                    $pl_code = $ar->pl_code;
-                    $sku = $ar->sku;
-                }
+        $export = [];
 
-                $date = date('d/m/Y H:i:s', strtotime($row->created_at));
-                $export[] = [
-                    $date,
-                    $row->st_name,
-                    $row->u_name,
-                    $sku,
-                    $br_name,
-                    $p_name,
-                    $p_color,
-                    $sz_name,
-                    $pl_code,
-                    $row->pmt_old_qty,
-                    $row->pmt_qty,
-                    ($row->pmt_old_qty - $row->pmt_qty),
-                    $row->pl_code
-                ];
-            }
+        foreach ($data as $row) {
+            $export[] = [
+                date('d/m/Y H:i:s', strtotime($row->created_at)),
+                $row->st_name,
+                $row->u_name,
+                $row->ps_barcode,
+                $row->br_name,
+                $row->p_name,
+                $row->p_color,
+                $row->sz_name,
+                $row->bin_start,
+                $row->pmt_old_qty,
+                $row->pmt_qty,
+                $row->bin_awal_setelah_mts,
+                $row->bin_tujuan,
+                $row->notes,
+            ];
         }
 
         return collect($export);
