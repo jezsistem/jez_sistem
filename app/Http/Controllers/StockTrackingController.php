0d9750ee9a5ee2f01338a279ccc768b08069b60f
JaluDwija37 <jaludwija37@gmail.com>
2025-07-16 11:30:56 +0700

    [StockTracking] - add order by clause to getPickupDatatables for better sorting

--- app/Http/Controllers/StockTrackingController.php
+++ app/Http/Controllers/StockTrackingController.php
@@ -379,7 +379,8 @@ class StockTrackingController extends Controller
                 ->whereIn('plst_status', $status)
                 ->where('product_location_setup_transactions.pst_id', '!=', null)
                 ->where('product_location_setup_transactions.st_id', '=', $st_id)
-                ->where('users.stt_id', '=', Auth::user()->stt_id))
+                ->where('users.stt_id', '=', Auth::user()->stt_id)
+                ->orderBy('product_location_setup_transactions.created_at', 'asc'))
                 ->editColumn('article', function ($data) {
                     return '<span style="white-space: nowrap; font-weight:bold;" class="btn btn-sm btn-primary">' . $data->p_name . ' ' . $data->p_color . ' [' . $data->sz_name . ']</span>';
                 })
