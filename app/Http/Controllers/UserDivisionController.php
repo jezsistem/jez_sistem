777f115dccbebf75ffec7e2ec518510fddac3388
unknown <muhammadroyyan11>
2025-08-29 15:37:59 +0700

    [Sidebar menu access] - Give permissions check at menu access user

--- app/Http/Controllers/UserDivisionController.php
+++ app/Http/Controllers/UserDivisionController.php
@@ -53,6 +53,8 @@ class UserDivisionController extends Controller
         
         $divisions = UserDivision::where('ud_status', 'active')->orderBy('ud_name')->get();
 
+        dd($this->sidebar());
+
         $data = [
             'title' => $title,
             'subtitle' => 'User Divisions',
