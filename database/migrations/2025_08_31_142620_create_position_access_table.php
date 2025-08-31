46e673832daeda559db408f3b4bc9c9282f8cff0
JaluDwija37 <jaludwija37@gmail.com>
2025-08-31 19:26:26 +0700

    Enhance position_access table migration with charset and collation settings

--- database/migrations/2025_08_31_142620_create_position_access_table.php
+++ database/migrations/2025_08_31_142620_create_position_access_table.php
@@ -16,10 +16,10 @@ class CreatePositionAccessTable extends Migration
         Schema::create('position_access', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('position_id');
-            $table->string('route');
-            $table->enum('action', ['create', 'read', 'update', 'delete']);
+            $table->string('route')->charset('utf8mb4')->collation('utf8mb4_general_ci');
+            $table->enum('action', ['create', 'read', 'update', 'delete'])->charset('utf8mb4')->collation('utf8mb4_general_ci');
             $table->timestamps();
-        });
+        }, 'utf8mb4', 'utf8mb4_general_ci');
     }
 
     /**
