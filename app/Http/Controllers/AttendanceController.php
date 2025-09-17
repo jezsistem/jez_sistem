0c14eebc99c71d6235e9a24dd9b1ed11f3d3d7ab
unknown <muhammadroyyan11>
2025-09-17 09:33:18 +0700

    [Schedule] - fix merge conflict

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -37,7 +37,7 @@ class AttendanceController extends Controller
             ->exists();
 
         if (!$user_group_is_admin  && !$is_human_resource) {
-            $check = DB::table('position_access.routeion_access')
+            $check = DB::table('position_access')
                 ->leftJoin('user_positions', 'user_positions.id', '=', 'position_access.position_id')->where([
                     'position_access.position_id' => $user_position,
 //                    'position_access.route' => request()->path()
