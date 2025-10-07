97c742d7b38a8d0ac4e8c01df10ae0e724bf785e
JaluDwija37 <jaludwija37@gmail.com>
2025-10-07 19:58:40 +0700

    [PO Approval]-  Rename "Tgl Barang Terima" to "Tgl Barang Datang" and add arrived_at field in modal

--- resources/views/app/po_approval/po_approval.blade.php
+++ resources/views/app/po_approval/po_approval.blade.php
@@ -146,7 +146,7 @@
                                             <th class="text-dark">Supplier</th>
                                             <th class="text-dark">Invoice</th>
                                             <th class="text-dark">Tgl Invoice</th>
-                                            <th class="text-dark">Tgl Barang Terima</th>
+                                            <th class="text-dark">Tgl Barang Datang</th>
                                             <th class="text-dark">Tgl Terima</th>
                                             <th class="text-dark">Penerima</th>
                                             <th class="text-dark">Approval</th>
