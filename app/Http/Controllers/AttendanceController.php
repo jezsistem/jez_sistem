3ad15431b036441fe7b32b6e6f97d09aec0117d9
Muhammad Royyan <royyanmz87@gmail.com>
2025-10-15 00:05:32 +0700

    [Attendance] - Permission Manual attendance

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -44,7 +44,7 @@ class AttendanceController extends Controller
                     //                    'position_access.route' => request()->path()
                 ])->exists();
 
-            if (!$check) {
+            if (!$check || Auth::user()->manual_attendance_access == 0) {
                 dd("Anda tidak memiliki akses ke menu ini, level Anda tidak dizinkan, hubungi Administrator");
             }
         }
