a0f5b2e59068ddd06765b19887a4384393cc3248
Muhammad Royyan <royyanmz87@gmail.com>
2025-11-03 21:12:11 +0700

    [Manifest] - Display manifest number

--- resources/views/app/delivery_recap/add_delivery_recap.blade.php
+++ resources/views/app/delivery_recap/add_delivery_recap.blade.php
@@ -180,6 +180,7 @@
     ClassicEditor
         .create(document.querySelector('#editor'), {
             toolbar: [
+                'heading', '|',
                 'bold', 'italic', 'bulletedList', 'numberedList', '|',
                 'undo', 'redo'
             ],
