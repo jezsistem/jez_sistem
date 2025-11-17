c54e53ea2f3c43bde94058b13e9ceacfea380765
yayanardhianto <yayanardhianto@users.noreply.github.com>
2025-11-17 19:34:52 +0700

    update pos v2

--- resources/views/app/pos_v2/pos_v2.blade.php
+++ resources/views/app/pos_v2/pos_v2.blade.php
@@ -600,7 +600,7 @@
                     <form id="f_shift_customer">
                         <!-- Button Group -->
                         <div class="flex gap-2 mb-4" role="group">
-                            <button type="button" id="start-shift-btn" class="flex-1 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            <button type="button" id="start-shift-btn" class="flex-1 text-white bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                 Start Shift
                             </button>
                             <button type="button" id="stop-shift-btn" class="hidden flex-1 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
